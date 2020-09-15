dddf836b23240591783761d9511876e77587ffc6
shawn han <hanshawn1995@gmail.com>
2020-09-15 12:47:08 +0900

    revise bugs

--- blog-app.py
+++ blog-app.py
@@ -111,12 +111,12 @@ class BlogPost(db.Model):
 
 class Customer(db.Model):
     __tablename__ = 'customer'
-    
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(200), nullable=False)
     phone = db.Column(db.String(200), nullable=False)
     message = db.Column(db.Text, nullable=True)
+
 
 
 @app.route("/customerInfo", methods=['GET', 'POST'])
@@ -139,17 +139,6 @@ def getcustm():
         return render_template('#', cust_db=allcust)
 
 
-
-class Customer(db.Model):    
-
-    id = db.Column(db.Integer, primary_key=True)
-    tel = db.Column(db.Integer)
-    name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(200), nullable=False)
-    message = db.Column(db.Text)
-
-    def __repr__(self):
-        return "Customer " + str(self.id) 
 
 @app.route("/") 
 def index():
@@ -174,26 +163,6 @@ def posts():
         # 如果不是添加新博客的操作， 则在posts页面显示所有博客
         all_posts = BlogPost.query.order_by(BlogPost.date_posted)
         return render_template('posts.html', post_db=all_posts)
-
-
-
-@app.route("/customer", methods=['GET', 'POST'])
-def customer():
-    # 获取contact人的信息
-    if request.method == 'POST':
-        customer_name = request.form['name']
-        customer_tel = request.form['phone']
-        customer_email = request.form['email']
-        customer_message = request.form['message']
-        new_cus = Customer(tel=customer_tel, name=customer_name, email=customer_email, message=customer_message)
-        db.session.add(new_cus)
-        db.session.commit()
-        return redirect('/index')
-    else:
-        all_cus = Customer.query.order_by(Customer.name)
-        return render_template(cust_db=all_cus)
-        #all_cus =  Customer.query.order_by(Customer.name)
-        #return render_template('customer.html', customer_db=all_cus)
 
 
 
@@ -284,7 +253,7 @@ def predict():
 if __name__ == "__main__":
     # 修改模板后立即生效
     #admin()
-    db.create_all()
+    #db.create_all()
     app.jinja_env.auto_reload = True
     app.run(port=2015, debug=True)
     
